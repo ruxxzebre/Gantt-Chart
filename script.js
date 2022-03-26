74c8a59fd567768e519da03122e2850ace54fc92
ruxxzebre <paullaboratory@gmail.com>
2022-03-26 19:28:44 +0200

    initial

---
+++ script.js
@@ -0,0 +1,106 @@
+google.charts.load('current', {'packages':['gantt']});
+google.charts.setOnLoadCallback(drawChart);
+
+function daysToMilliseconds(days) {
+    return days * 24 * 60 * 60 * 1000;
+}
+
+function strToDate(date) {
+    if (date instanceof Date) return date;
+    const s = date.split('.').map(i => parseFloat(i));
+    return new Date(s[2], s[1] + 1, s[0])
+}
+
+// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+/**
+ *
+ * @param {Date | string} start
+ * @param {Date | string} end
+ */
+function getDuration(start, end, toMs = false) {
+    start = strToDate(start);
+    end = strToDate(end);
+    const b = start.getDate()*(start.getMonth() + 1);
+    const f = start.getDate()*(start.getMonth() + 1);
+    return (f - b);
+}
+
+/**
+ *
+ * @param {string} id
+ * @param {string} name
+ * @param {string} start - in DD.MM.YYYY form
+ * @param {string} end - in DD.MM.YYYY form
+ * @param {number} percentage - from 0 to 100
+ * @param {number} duration - in days
+ * @param {string} deps
+ */
+function makeRow(name, start, end, percentage = 0, duration = null, deps = null) {
+
+    if (!duration) duration = getDuration(start, end);
+    const id = generateUUID();
+    return {raw: [
+        id,
+        name,
+        strToDate(start),
+        strToDate(end),
+        daysToMilliseconds(duration),
+        percentage,
+        deps,
+    ], id};
+}
+
+function drawChart() {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
+
+    const __rows = [        makeRow('Preparing/discussing requirements',
+        '20.02.2022',
+        '23.02.2022', 100),
+        makeRow('Approving requirements', '23.02.2022', '26.02.2022', 100),
+        makeRow('Onboarding/discussing design', '26.02.2022', '28.02.2022', 100),
+        makeRow('Design phase', '01.03.2022', '16.03.2022', 100),
+        makeRow('Development/Testing phase (SCRUM)', '17.03.2022', '26.05.2022', 69),
+        makeRow('Pre release testing', '01.06.2022', '15.06.2022'),
+        makeRow('Preparing for release/Product polishing', '16.06.2022', '28.07.2022'),
+        makeRow('Release phase', '2.07.2022', '29.08.2022'),
+    ].map((i, idx, arr) => {
+        if (!idx) return i.raw;
+        i.raw[i.raw.length - 1] = arr[idx - 1].id;
+        return i.raw;
+    });
+    console.log(__rows);
+
+    data.addRows(__rows);
+
+    const options = {
+        height: 500
+    };
+
+    const chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
